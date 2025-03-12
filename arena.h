8f36cf6f0fb8b83fb7be1e5daea3b1c37dd7fe90
Abdelrahman Sarhan <abdulrahmanayman1@gmail.com>
2025-03-12 01:27:44 -0400

    Improved Arena API, Added ResetRegion() Function.

--- arena.h
+++ arena.h
@@ -1,5 +1,6 @@
 #ifndef ARENA_H
 #define ARENA_H
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -18,19 +19,19 @@ static inline Arena InitArena(size_t size) {
     arena.arenaPtr = malloc(size);
     if (arena.arenaPtr == NULL) {
         fprintf(stderr, "ERROR: Failed to allocate memory for arena!\n");
-        exit(1);
+        exit(-1);
     }
     arena.previousAllocs = 0;
     return arena;
 }
 
-static inline void* ArenaAlloc(Arena* arena, size_t allocSize) {
-    if (arena->previousAllocs + allocSize > arena->arenaSize) {
+static inline void* ArenaAlloc(Arena& arena, size_t allocSize) {
+    if (arena.previousAllocs + allocSize > arena.arenaSize) {
         fprintf(stderr, "ERROR: Allocation exceeds the size of the arena!");
-        exit(1);
+        exit(-1);
     }
-    void* alloc = (char*)arena->arenaPtr + arena->previousAllocs;
-    arena->previousAllocs += allocSize;
+    void* alloc = (char*)arena.arenaPtr + arena.previousAllocs;
+    arena.previousAllocs += allocSize;
     return alloc;
 }
 
@@ -40,7 +41,24 @@ static inline void ResetArena(Arena* arena) {
 }
 
 static inline void FreeArena(Arena* arena) {
-    free(arena->arenaPtr);
-    arena->arenaPtr = NULL;
+    if (arena->arenaPtr != NULL) {
+        free(arena->arenaPtr);
+        arena->arenaPtr = NULL;
+    }
+    else {
+        fprintf(stderr, "ERROR: Arena is null, not going to free arena.");
+    }
 }
-#endif //ARENA_H
+static inline int ResetRegion(Arena arena, void* region_start, size_t region_end) {
+    if (arena.arenaPtr) {
+        memset(region_start, 0, region_end);
+        fprintf(stdout, "INFO: region reset successfully!");
+        return 0;
+    }
+    else {
+        fprintf(stderr, "ERROR: Arena is null, not going to reset region.");
+        return -1;
+    }
+}
+
+#endif // ARENA_H
